4538d8ac17198e62ee1dd2bad70578c55aafcb8b
nicole <1251501326@qq.com>
2021-03-05 15:57:48 +0800

    创建 views 目录

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,19 @@
 import React from 'react';
-import Nav from 'components/Nav'
 import {
   HashRouter as Router,
   Switch,
   Route,
   Redirect
-} from "react-router-dom";
-import styled from 'styled-components';
-
-const Wrapper =styled.div`
-  height: 100vh;
-  display: flex;
-  flex-direction: column; 
-`;
-const Main =styled.div`
-  flex-grow: 1;
-  overflow: auto;
-`;
+} from 'react-router-dom';
+import Tags from 'views/Tags';
+import Money from 'views/Money';
+import Statistics from 'views/Statistics';
+import NoMatch from 'views/NoMatch';
 
 function App() {
   return (
     <Router>
-      <Wrapper>
-        <Main>
+
           <Switch>
             <Route path="/tags">
               <Tags/>
@@ -40,33 +31,9 @@ function App() {
             //设置默认路由
             <Redirect exact from="/" to="/money"/>
           </Switch>
-        </Main>
-        <Nav/>
-      </Wrapper>
+
     </Router>
   );
 }
-
-function Statistics() {
-  return <h2>统计页面</h2>;
-}
-
-function Tags() {
-  return <h2>标签页面</h2>;
-}
-
-function Money() {
-  return <h2>记账页面</h2>;
-}
-function NoMatch() {
-  return (
-    <div>
-      <h3>
-        当前页面不存在，请重新输入地址！
-      </h3>
-    </div>
-  );
-}
-
 export default App;
 
---
+++ src/views/NoMatch.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function NoMatch() {
+  return (
+    <div>
+      <h3>
+        当前页面不存在，请重新输入地址！
+      </h3>
+    </div>
+  );
+}
+
+export default NoMatch;

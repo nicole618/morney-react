2a2ec6c12acd4709fc12067fdda8f45f76d27db4
nicole <1251501326@qq.com>
2021-03-05 15:31:36 +0800

    创建Icon组群

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,9 +1,7 @@
 import styled from 'styled-components';
 import {Link} from 'react-router-dom';
 import React from 'react';
-require('icons/money.svg');
-require('icons/tag.svg');
-require('icons/chart.svg');
+import Icon from './Icon';
 
 
 const NavWrapper =styled.nav`
@@ -34,21 +32,15 @@ const Nav = ()=>{
     <NavWrapper>
       <ul>
         <li>
-          <svg className="icon">
-            <use xlinkHref="#tag"></use>
-          </svg>
+          <Icon name="tag"/>
           <Link to="/tags">标签</Link>
         </li>
         <li>
-          <svg className="icon">
-            <use xlinkHref="#money"></use>
-          </svg>
+          <Icon name="money"/>
           <Link to="/money">记一笔</Link>
         </li>
         <li>
-          <svg className="icon">
-            <use xlinkHref="#chart"></use>
-          </svg>
+          <Icon name="chart"/>
           <Link to="/statistics">统计</Link>
         </li>
       </ul>

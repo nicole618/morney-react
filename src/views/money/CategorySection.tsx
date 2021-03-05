d211d45a04b68c36c2ec7d6596817085b78b6b75
nicole <1251501326@qq.com>
2021-03-05 21:39:52 +0800

    收入支出tab标签切换

--- src/views/money/CategorySection.tsx
+++ src/views/money/CategorySection.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
+import React, {useState} from 'react';
 
-const CategorySection = styled.section`
+const Wrapper = styled.section`
   >ul{
     display: flex;
     background: #c4c4c4;
@@ -23,5 +24,20 @@ const CategorySection = styled.section`
     }
   }
 `;
-
+const CategorySection:React.FC = ()=>{
+  const categoryMap = {'-':'支出','+':'收入'};
+  const [categoryList] = useState<('-'|'+')[]>(['-','+']);
+  const [category,setCategory] = useState('-');
+  return (
+    <Wrapper>
+      <ul>
+        {categoryList.map(c=>
+            <li className = { category === c ? 'selected':'' }
+                onClick={()=>setCategory(c)}>{categoryMap[c]}
+            </li>
+        )}
+      </ul>
+    </Wrapper>
+  )
+}
 export {CategorySection};

85e3121b77533a0da2252c95b7816d03b6ce665a
nicole <1251501326@qq.com>
2021-03-05 22:29:55 +0800

    numberPad wip

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -16,30 +16,7 @@ function Money() {
       <TagsSection/>
       <NoteSection/>
       <CategorySection/>
-      <NumberSection>
-        <div className="output">100</div>
-        <div className="pad clearfix">
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>+</button>
-          <button>删除</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>-</button>
-          <button>清空</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>×</button>
-          <button className="ok">OK</button>
-          <button className="zero">0</button>
-          <button>.</button>
-          <button>%</button>
-          <button>÷</button>
-        </div>
-      </NumberSection>
+      <NumberSection />
     </MyLayout>
   );
 }
--- src/views/money/NumberSection.tsx
+++ src/views/money/NumberSection.tsx
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
+import React, {useState} from 'react';
+import {setState} from 'jest-circus/build/state';
 
-const NumberSection = styled.section`
+const Wrapper = styled.section`
   display: flex;
   flex-direction: column;
   >.output{
@@ -15,7 +17,7 @@ const NumberSection = styled.section`
     >button{
       font-size: 18px;
       float: left;
-      width: 20%;
+      width: 25%;
       height: 64px;
       border: none;
       &.ok{
@@ -26,25 +28,77 @@ const NumberSection = styled.section`
      &:first-child{
        background: #F2F2F2;
      } 
-      &:nth-child(2),&:nth-child(6){
+      &:nth-child(2),&:nth-child(5){
         background: #E0E0E0;
       }
-      &:nth-child(3),&:nth-child(7),&:nth-child(11){
+      &:nth-child(3),&:nth-child(6),&:nth-child(9){
         background: #D3D3D3;
       }
-      &:nth-child(4),&:nth-child(8),&:nth-child(12),&:nth-child(16){
+      &:nth-child(4),&:nth-child(7),&:nth-child(10),&:nth-child(13){
         background: #C1C1C1;
       }
-      &:nth-child(5),&:nth-child(9),&:nth-child(13),&:nth-child(17){
+      &:nth-child(8),&:nth-child(11),&:nth-child(14){
         background: #B8B8B8;
       }
-      &:nth-child(10),&:nth-child(14),&:nth-child(18){
+      &:nth-child(14){
         background: #A9A9A9;
       }
-      &:nth-child(19){
-        background: #9A9A9A;
+      &.zero{
+        width: 50%;
       }
     }
   }
 `
+const NumberSection:React.FC = ()=>{
+  const [output,setOutput] = useState<string>('0');
+  const onClickButtonWrapper = (e:React.MouseEvent)=>{
+     const text = (e.target as HTMLButtonElement).textContent;
+     if(text===null)return;
+     switch (text){
+       case '0':
+       case '1':
+       case '2':
+       case '3':
+       case '4':
+       case '5':
+       case '6':
+       case '7':
+       case '8':
+       case '9':
+       case '.':
+         output==='0'?setOutput(text):setOutput(output+text)
+         break;
+       case '删除':
+         console.log('删除')
+         break;
+       case '清空':
+         console.log('清空')
+         break;
+       case 'ok':
+         console.log('确认')
+         break;
+     }
+  }
+  return(
+   <Wrapper>
+     <div className="output">{output}</div>
+     <div className="pad clearfix" onClick={onClickButtonWrapper}>
+       <button>1</button>
+       <button>2</button>
+       <button>3</button>
+       <button>删除</button>
+       <button>4</button>
+       <button>5</button>
+       <button>6</button>
+       <button>清空</button>
+       <button>7</button>
+       <button>8</button>
+       <button>9</button>
+       <button className="ok">OK</button>
+       <button className="zero">0</button>
+       <button>.</button>
+     </div>
+   </Wrapper>
+  )
+}
 export {NumberSection}

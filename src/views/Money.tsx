0e6042ee27c889766576e7ed6c41313a57925eb5
nicole <1251501326@qq.com>
2021-03-05 18:10:17 +0800

    Money.tsx 之 CategorySection UI 完成

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -37,7 +37,29 @@ const NotesSection = styled.section`
     }
   }
 `;
-const CategorySection = styled.section``;
+const CategorySection = styled.section`
+  >ul{
+    display: flex;
+    background: #c4c4c4;
+    font-size: 24px;
+    >li{
+      width: 50%;
+      text-align: center;
+      padding: 16px 0;
+      position: relative;
+      &.selected::after{
+        content: '';
+        display: block;
+        height: 3px;
+        background: #333;
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+      }
+    }
+  }
+`;
 const NumberSection = styled.section``;
 function Money() {
   return (
@@ -59,7 +81,7 @@ function Money() {
       </NotesSection>
       <CategorySection>
         <ul>
-          <li>支出</li>
+          <li className="selected">支出</li>
           <li>收入</li>
         </ul>
       </CategorySection>

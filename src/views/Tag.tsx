1e1cc1cc0934b5865792fa238d2496f0be7f6fa5
nicole <1251501326@qq.com>
2021-03-07 12:02:39 +0800

    创建Button组件

--- src/views/Tag.tsx
+++ src/views/Tag.tsx
@@ -1,17 +1,47 @@
 import React from 'react';
 import {useTags} from 'useTags';
 import {useParams} from 'react-router-dom'
+import Layout from 'components/Layout';
+import Icon from 'components/Icon';
+import {Button} from '../components/Button';
+import styled from 'styled-components';
 
 type Params = {
    id : string
 }
+const TopBar = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  line-height: 20px;
+  padding: 14px;
+  background: #fff;
+  .icon{
+    width: 25px;
+    height: 25px;
+    fill:#999;
+  }
+`;
 const Tag:React.FC = (props)=>{
    const {findTag} = useTags();
    let { id } = useParams<Params>()
    const tag = findTag(parseInt(id));
    return(
-     <div>{tag.name}</div>
+     <Layout>
+        <TopBar>
+          <Icon name = "left"/>
+          <span>编辑标签</span>
+          <Icon/>
+        </TopBar>
+       <div>
+
+       </div>
+       <div>
+         <Button>删除标签</Button>
+       </div>
+     </Layout>
    )
 }
 
 export {Tag}
+
--- src/views/money/NoteSection.tsx
+++ src/views/money/NoteSection.tsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
-import React, {useRef} from 'react';
+import React, {ChangeEventHandler, useRef} from 'react';
+import {Input} from '../../components/Input';
 
 const Wrapper = styled.section`
   background: #f5f5f5;
@@ -27,15 +28,15 @@ type  Props = {
 }
 const NoteSection:React.FC<Props> = (props)=>{
   const note = props.value;
-  const refInput = useRef<HTMLInputElement>(null);
-  const onBlur = ()=>{
-    if(refInput.current !==null)props.onChange(refInput.current.value)
+  const onChange:ChangeEventHandler<HTMLInputElement> = (e)=>{
+    props.onChange(e.target.value)
   }
   return(
     <Wrapper>
       <label>
         <span>备注</span>
-        <input type="text" defaultValue={note} ref={refInput} onBlur={onBlur} placeholder="在这里添加备注"/>
+        {/*<input type="text" defaultValue={note} ref={refInput} onChange={onChange} placeholder="在这里添加备注"/>*/}
+        <Input label="备注" type="text" value={note}  onChange={onChange} placeholder="在这里添加备注"/>
       </label>
     </Wrapper>
   )

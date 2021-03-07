fa5d8794b45cfb0e0eb54853ea8d1f320bb6c4ef
nicole <1251501326@qq.com>
2021-03-07 22:09:15 +0800

    优化滚动逻辑

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,5 @@
 import Nav from './Nav';
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import styled from 'styled-components';
 
 const Wrapper =styled.div`
@@ -11,16 +11,30 @@ const Main =styled.div`
   flex-grow: 1;
   overflow: auto;
 `;
+type Props = {
+  className?:string;
+  scrollTop?:number;
+}
+const Layout:React.FC<Props> = (props) =>{
+  const mainRef = useRef<HTMLDivElement>(null);
 
-const Layout = (props:any) =>{
+  useEffect(()=>{
+    setTimeout(()=>{
+      if(!mainRef.current){return;}
+      mainRef.current.scrollTop = props.scrollTop!;
+    },0)
+
+  },[props.scrollTop])
   return (
     <Wrapper>
-      <Main className={props.className}>
+      <Main ref={mainRef} className={props.className}>
         {props.children}
       </Main>
       <Nav/>
     </Wrapper>
   )
 }
-
+Layout.defaultProps ={
+  scrollTop:0
+}
 export default Layout
--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -44,7 +44,7 @@ function Money() {
   };
 
   return (
-    <MyLayout>
+    <MyLayout scrollTop={99999}>
       <TagsSection value={selected.tagIds}
                    onChange={tagIds => onChange({tagIds})}/>
       <NoteSection value={selected.note}
